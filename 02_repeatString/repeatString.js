cd188a8c375ce935bf37770a5b1e2d288c692883
Strahinja Jekic <144447767+Straxxor@users.noreply.github.com>
2023-10-31 15:10:05 +0100

    Started doing the exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string1, numberoftimes) {
+if (numberoftimes>=0) {
+    return string1.repeat(numberoftimes);
+} else {
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
